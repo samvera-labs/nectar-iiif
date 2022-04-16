db4289353edaab728ddd046839281db2ab467e2b
Mat Jordan <mat@northwestern.edu>
2022-04-16 08:46:10 -0400

    Tidy up language select dev components.

--- src/dev.tsx
+++ src/dev.tsx
@@ -3,13 +3,14 @@ import { Vault } from "@iiif/vault";
 import ReactDOM from "react-dom";
 import { ManifestNormalized } from "@iiif/presentation-3";
 import { Label, Metadata, RequiredStatement, Summary } from "./index";
+import Language from "./dev/Language";
 
 const Wrapper = () => {
   const [manifest, setManifest] = useState<ManifestNormalized>();
   const [language, setLanguage] = useState<String | undefined>();
 
   const manifestId =
-    "http://127.0.0.1:8080/fixtures/manifest/internationalization.json";
+    "https://iiif.io/api/cookbook/recipe/0006-text-language/manifest.json";
 
   useEffect(() => {
     const vault = new Vault();
@@ -22,24 +23,16 @@ const Wrapper = () => {
         });
   }, [manifestId]);
 
-  const handleLanguage = (e) => {
-    const { value } = e.target;
-    setLanguage(value !== "--" ? value : undefined);
-  };
-
   if (!manifest) return <>Loading...</>;
+
+  const handleLanguage = (e) =>
+    setLanguage(e.target.value !== "--" ? e.target.value : undefined);
 
   const { label, summary, metadata, requiredStatement } = manifest;
 
   return (
     <>
-      <label htmlFor="language-code">Language Code</label>
-      <select id="language-code" name="language-code" onChange={handleLanguage}>
-        <option>--</option>
-        <option value="none">none</option>
-        <option value="en">en</option>
-        <option value="fr">fr</option>
-      </select>
+      <Language handleLanguage={handleLanguage} />
       <div>
         <Label label={label} as="h1" language={language} />
         <Summary summary={summary} language={language} />
---
+++ src/dev/Language.tsx
@@ -0,0 +1,20 @@
+import React, { SyntheticEvent } from "react";
+
+interface LanguageProps {
+  handleLanguage: (e: SyntheticEvent) => void;
+}
+const Language: React.FC<LanguageProps> = ({ handleLanguage }) => {
+  return (
+    <form>
+      <label htmlFor="language-code">Language Code</label>
+      <select id="language-code" name="language-code" onChange={handleLanguage}>
+        <option>--</option>
+        <option value="none">none</option>
+        <option value="en">en</option>
+        <option value="fr">fr</option>
+      </select>
+    </form>
+  );
+};
+
+export default Language;
