342e6590e22c4a931f7d6f2df1b7c09be625d899
Mat Jordan <mat@northwestern.edu>
2022-04-19 22:16:10 -0400

    Refine resource hook.

--- src/hooks/useGetImageResource.ts
+++ src/hooks/useGetImageResource.ts
@@ -1,24 +1,32 @@
-import { IIIFExternalWebResource } from "@iiif/presentation-3";
+import { IIIFExternalWebResource, ImageService } from "@iiif/presentation-3";
 
 export const useGetImageResource = (
   resource: IIIFExternalWebResource,
   size: string = "200,",
   region: string = "full"
 ) => {
+  /**
+   * defenseively ensure resource is not an array
+   */
   if (Array.isArray(resource)) resource = resource[0];
 
-  if (!resource.service) return resource.id;
+  const { id, service } = resource;
 
-  if (!Array.isArray(resource.service)) {
-    if (resource.service["@id"])
-      return `${resource.service["@id"]}/${region}/${size}/0/default.jpg`;
+  let imageService: ImageService | undefined;
 
-    if (resource.service.id)
-      return `${resource.service.id}/${region}/${size}/0/default.jpg`;
+  /**
+   * return resource id if service does not exist
+   */
+  if (!service) return id;
+
+  if (Array.isArray(resource.service) && resource.service.length > 0)
+    imageService = service[0] as ImageService;
+
+  if (imageService) {
+    if (imageService["@id"])
+      return `${imageService["@id"]}/${region}/${size}/0/default.jpg`;
+
+    if (imageService.id)
+      return `${imageService.id}/${region}/${size}/0/default.jpg`;
   }
-
-  if (resource.service[0]["@id"])
-    return `${resource.service[0]["@id"]}/${region}/${size}/0/default.jpg`;
-
-  return `${resource.service[0].id}/${region}/${size}/0/default.jpg`;
 };
